4f0fbebad4b056961eb1eba9d9432a145797aff3
Wisnu ST <wisnust10@gmail.com>
2017-01-17 22:44:25 +0700

    Creating sidebar on mobile

--- js/custom.js
+++ js/custom.js
@@ -1,16 +1,25 @@
 $(document).ready(function() {
 
 
-    // Style guide nav
+    // Navbar mobile
+    var $topNav = $('.navbar-hypescreen .navbar-nav'),
+        $sideNav = $('.sidenav nav'),
+        $logoHeight = $('.navbar-hypescreen').height(),
+        $topNav_height = $topNav.height();
+
+    $topNav.css('top', $logoHeight);
+    $sideNav.css('top', $topNav_height + $logoHeight);
+
+
     $('#show-nav-sg').click(function(event) {
-      $('#nav-sg').toggleClass('normal');
-      $('#nav-backdrop').fadeToggle(400);
+        $topNav.add($sideNav).toggleClass('nav-open');
+        // $('#nav-backdrop').fadeToggle(400);
     });
 
-    $('#nav-backdrop').click(function(event) {
-      $('#nav-sg').toggleClass('normal');
-      $(this).fadeToggle(400);
-    }); 
+    // $('#nav-backdrop').click(function(event) {
+    //     $topNav.add($sideNav).toggleClass('nav-open');
+    //     $(this).fadeToggle(400);
+    // }); 
 
 
 });
